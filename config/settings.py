123a2d6d20df87c7f9124153d274b9f488b096ac
tenid <40762733+tenid@users.noreply.github.com>
2021-02-22 01:04:42 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gbv5!km8%m@7#x)=b9iwwaby8-jlkv-kl8i)+x1odr=$o4z@dd'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
